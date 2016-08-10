d244a397bbf60d5ba4e4a9a89f68bb9d0ce60ac6
Robert Mitchem <bmitchem@gmail.com>
2016-08-10 14:33:49 -0400

    removed duplicate uwsgi

--- UBERATCapp/wsgi.py
+++ UBERATCapp/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UBERATCapp.settings")
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "UBERATCapp.settings")
 
-application = get_wsgi_application()
+    application = get_wsgi_application()
+
