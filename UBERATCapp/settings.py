4363544f2317624bd2b1d7c1b16136ce56df8216
Robert Mitchem <bmitchem@gmail.com>
2016-08-10 13:10:40 -0400

    fixing static files

--- UBERATCapp/settings.py
+++ UBERATCapp/settings.py
@@ -150,6 +150,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
