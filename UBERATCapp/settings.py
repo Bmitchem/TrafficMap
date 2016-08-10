8f653e50492eff8ff96551a9d6cf4119f7e93e6f
Robert Mitchem <bmitchem@gmail.com>
2016-08-10 15:57:29 -0400

    fixed uwsgi

--- UBERATCapp/settings.py
+++ UBERATCapp/settings.py
@@ -159,6 +159,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "bower_components")
 ]
