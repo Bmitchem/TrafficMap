6db31c9a03cb1ce8e729969310009c74c8bff7ec
Robert Mitchem <bmitchem@gmail.com>
2016-08-10 20:43:52 -0400

    fixed uwsgi

--- UBERATCapp/settings.py
+++ UBERATCapp/settings.py
@@ -137,37 +137,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'standard': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins',],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
