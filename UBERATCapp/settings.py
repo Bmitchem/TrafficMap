afb34f9df5072fddb892f6794e8e8e5b8cc9ab5f
Robert Mitchem <bmitchem@gmail.com>
2016-08-10 20:36:38 -0400

    fixed uwsgi

--- UBERATCapp/settings.py
+++ UBERATCapp/settings.py
@@ -187,9 +187,9 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "bower_components")
